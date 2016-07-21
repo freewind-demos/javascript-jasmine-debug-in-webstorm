faf0a712cbec7188582782b8a18262df6a4f354f
Peng.Li <peng.li@rea-group.com>
2016-07-21 22:13:55 +0800

    Use runner provided by Jasmine, replace node-jasmine

--- run-spec.js
+++ run-spec.js
@@ -1,6 +1,28 @@
-var jasmine = require('jasmine-node');
-jasmine.executeSpecsInFolder({
-  specFolders: ['spec'],
-  isVerbose: true,
-  showColors: true
+var Jasmine = require('jasmine');
+var jasmine = new Jasmine();
+
+jasmine.loadConfig({
+  spec_dir: 'spec',
+  spec_files: [
+    '*.js'
+  ],
+  helpers: [
+    'helpers/**/*.js'
+  ],
+  stopSpecOnExpectationFailure: false,
+  random: false
 });
+
+// A 3rd-party reporter
+var JasmineConsoleReporter = require('jasmine-console-reporter');
+var reporter = new JasmineConsoleReporter({
+  colors: 1,           // (0|false)|(1|true)|2
+  cleanStack: 1,       // (0|false)|(1|true)|2|3
+  verbosity: 4,        // (0|false)|1|2|(3|true)|4
+  listStyle: 'indent', // "flat"|"indent"
+  activity: false
+});
+
+jasmine.addReporter(reporter);
+
+jasmine.execute();
